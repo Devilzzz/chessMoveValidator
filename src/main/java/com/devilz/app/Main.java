f8dedd6c3c570d226cfbe4ef97f66d9b98b61beb
Ankit Gupta <ankit.gupta@lendingkart.com>
2018-09-07 17:48:20 +0530

    Added move inputs and escape sequence

--- src/main/java/com/devilz/app/Main.java
+++ src/main/java/com/devilz/app/Main.java
@@ -2,12 +2,23 @@ package com.devilz.app;
 
 import com.devilz.validator.Validator;
 
+import java.util.Scanner;
+
 public class Main {
 
     private static final String WELCOME_MESSAGE = "Welcome to chess move validator";
 
     public static void main(String[] args){
+        //initializing new validator
         System.out.println(WELCOME_MESSAGE);
         Validator validator = new Validator();
+
+        //reading input
+        Scanner in = new Scanner(System.in);
+        String temp;
+        while(true){
+            temp = in.nextLine();
+            if(temp.equals("exit"))break;
+        }
     }
 }
